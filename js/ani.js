4e617598f9054692fa49f596bdeb6257851e4d6b
heejin99 <wkdgmlwls30@naver.com>
2021-06-02 14:27:14 +0900

    포트폴리오 전면 수정

---
+++ js/ani.js
@@ -0,0 +1,35 @@
+$('.strip').each(function(){
+    var $t = $(this),
+        rows = $.trim($t.html()).split('<br>');
+  
+    $t.html('');
+  
+    $.each(rows, function(i, val){
+        $('<span class="row"></span>').appendTo($t);
+    
+        var letters = $.trim(val).split('');
+    
+        $.each(letters, function(j, v){
+            v = (v == ' ') ? '&nbsp;' : v;
+            $('<span>' + $.trim(v) + '</span>').appendTo($('.row:last', $t));
+        });
+    });
+});
+  
+$('body').each(function(){
+    for (i = 0; i < $('.strip span').length; i++) {
+        (function(ind) {
+            setTimeout(function(){
+                $('.strip span:not(".row")').eq(ind).toggleClass('animate');
+            }, ind * 15);
+        })(i);
+    }
+}).click();
+  
+  
+function openNav() {
+    document.getElementById('mysidenav').style.width = '250px';
+}
+function closeNav() {
+    document.getElementById('mysidenav').style.width = '0';
+}
